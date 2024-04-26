7aac260ba830ce4f72f29e3aa089e39b920731cc
ariefrahmansyah <ariefrahmansyah@hotmail.com>
2024-04-26 21:18:35 +0700

    - Install shadcn ui - Add header - Modify footer - Modify homepage to coming soon
    Co-authored-by: fadelun <fadeljourney@gmail.com>
    Co-authored-by: rbernanda <acappemenang@gmail.com>

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,30 +1,83 @@
-/** @see https://tailwindcss.com/docs/configuration */
 /** @type {import('tailwindcss').Config} */
 module.exports = {
+  darkMode: ["class"],
   content: [
-    './src/packages/**/*.{js,ts,jsx,tsx,mdx}',
-    './src/modules/**/*.{js,jsx,ts,tsx,mdx}',
-    './src/app/**/*.{js,ts,jsx,tsx,mdx}'
+    "./src/packages/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/modules/**/*.{js,jsx,ts,tsx,mdx}",
+    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
+  prefix: "",
   theme: {
+    container: {
+      center: true,
+      padding: "2rem",
+      screens: {
+        "2xl": "1400px",
+      },
+    },
     fontFamily: {
       'sans': ['var(--font-inter)', 'ui-sans-serif', 'system-ui'],
       'serif': ['var(--font-inter)', 'ui-serif', 'Georgia'],
       'mono': ['var(--font-inter)', 'ui-monospace', 'SFMono-Regular']
     },
     extend: {
-      keyframes: require('./design/tailwind/keyframes'),
+      colors: {
+        border: "hsl(var(--border))",
+        input: "hsl(var(--input))",
+        ring: "hsl(var(--ring))",
+        background: "hsl(var(--background))",
+        foreground: "hsl(var(--foreground))",
+        primary: {
+          DEFAULT: "hsl(var(--primary))",
+          foreground: "hsl(var(--primary-foreground))",
+        },
+        secondary: {
+          DEFAULT: "hsl(var(--secondary))",
+          foreground: "hsl(var(--secondary-foreground))",
+        },
+        destructive: {
+          DEFAULT: "hsl(var(--destructive))",
+          foreground: "hsl(var(--destructive-foreground))",
+        },
+        muted: {
+          DEFAULT: "hsl(var(--muted))",
+          foreground: "hsl(var(--muted-foreground))",
+        },
+        accent: {
+          DEFAULT: "hsl(var(--accent))",
+          foreground: "hsl(var(--accent-foreground))",
+        },
+        popover: {
+          DEFAULT: "hsl(var(--popover))",
+          foreground: "hsl(var(--popover-foreground))",
+        },
+        card: {
+          DEFAULT: "hsl(var(--card))",
+          foreground: "hsl(var(--card-foreground))",
+        },
+      },
+      borderRadius: {
+        lg: "var(--radius)",
+        md: "calc(var(--radius) - 2px)",
+        sm: "calc(var(--radius) - 4px)",
+      },
+      keyframes: require("./design/tailwind/keyframes"),
+      animation: {
+        "accordion-down": "accordion-down 0.2s ease-out",
+        "accordion-up": "accordion-up 0.2s ease-out",
+      },
       backgroundImage: {
         'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
         'gradient-conic':
           'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))'
       }
-    }
+    },
   },
   plugins: [
-    require('@tailwindcss/typography'),
-    require('daisyui'),
-    require('./design/tailwind/custom.tailwind')
+    require("tailwindcss-animate"),
+    require("@tailwindcss/typography"),
+    require("daisyui"),
+    require("./design/tailwind/custom.tailwind"),
   ],
   daisyui: require('./design/daisyui/daisyui.config')
 };
