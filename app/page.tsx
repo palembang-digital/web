b453f28ae7cce6ee1a99ac422cbfad41e7627060
ariefrahmansyah <ariefrahmansyah@hotmail.com>
2024-07-04 23:42:30 +0700

    display events

--- app/page.tsx
+++ app/page.tsx
@@ -2,13 +2,16 @@ import EventsGrid from "@/components/events/events-grid";
 import Footer from "@/components/footer";
 import Header from "@/components/header";
 import Hero from "@/components/hero";
+import EventsModel from "@/models/events";
 
-export default function Page() {
+export default async function Page() {
+  const events = await EventsModel.findAll();
+
   return (
     <>
       <Header />
       <Hero />
-      <EventsGrid />
+      <EventsGrid events={events} end={5} />
       <Footer />
     </>
   );
--- components/events/events-grid.tsx
+++ components/events/events-grid.tsx
@@ -1,5 +1,52 @@
-function EventsGrid() {
-  return <div className="flex gap-6"></div>;
+import { Card, CardContent, CardHeader } from "@/components/ui/card";
+import { events as eventsSchema } from "@/db/schema/events";
+import { InferSelectModel } from "drizzle-orm";
+import Image from "next/image";
+import Link from "next/link";
+
+function EventsGrid({
+  events,
+  start,
+  end,
+}: {
+  events: InferSelectModel<typeof eventsSchema>[];
+  start?: number;
+  end?: number;
+}) {
+  return (
+    <div className="flex gap-6">
+      {events
+        .sort(
+          (a, b) =>
+            Date.now() -
+            new Date(a.scheduledStart).getTime() -
+            (Date.now() - new Date(b.scheduledStart).getTime())
+        )
+        .slice(start, end)
+        .map((event, idx) => (
+          <Link key={idx} href={`/events/${event.id}`}>
+            <Card className="w-[200px]">
+              <CardHeader>
+                <Image
+                  className="object-cover "
+                  src={event.imageUrl || ""}
+                  width={200}
+                  height={200}
+                  alt={event.name}
+                />
+              </CardHeader>
+              <CardContent className="text-muted-foreground flex flex-col text-sm">
+                {/* <p>{FormatDateEvent(event.scheduledEnd)}</p> */}
+                <p className="truncate text-lg font-bold text-black">
+                  {event.name}
+                </p>
+                <p>tempat</p>
+              </CardContent>
+            </Card>
+          </Link>
+        ))}
+    </div>
+  );
 }
 
 export default EventsGrid;
