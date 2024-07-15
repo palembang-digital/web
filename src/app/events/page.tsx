106546008f0cbe3bc8fbef5b4be4bbf6ea934030
Arief Rahmansyah <ariefrahmansyah@hotmail.com>
2024-07-15 20:44:05 +0700

    fix: missing session

--- src/app/events/page.tsx
+++ src/app/events/page.tsx
@@ -1,16 +1,19 @@
+import { auth } from "@/auth";
 import { FloatingHeader } from "@/components/floating-header";
 import { ScrollArea } from "@/components/scroll-area";
 import UpcomingEvents from "@/components/upcoming-events";
 import { db } from "@/db";
 
 export default async function Page() {
+  const session = await auth();
+
   const events = await db.query.events.findMany({
     orderBy: (events, { desc }) => [desc(events.scheduledStart)],
   });
 
   return (
     <ScrollArea useScrollAreaId>
-      <FloatingHeader scrollTitle="Kegiatan Patal" />
+      <FloatingHeader session={session} scrollTitle="Kegiatan Patal" />
       <div className="content-wrapper">
         <div className="content">
           <UpcomingEvents events={events} />
