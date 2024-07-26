d2f6d8d1f908387a1b669e53f1f012185eb847e8
Muhammad Fadll <65841599+fadelun@users.noreply.github.com>
2024-07-26 20:56:29 +0700

    add event card component (#87)

--- src/components/past-events.tsx
+++ src/components/past-events.tsx
@@ -1,3 +1,4 @@
+import EventCard from "@/components/events/event-card";
 import { Button } from "@/components/ui/button";
 import { TypographyH2, TypographyH3 } from "@/components/ui/typography";
 import Link from "next/link";
@@ -12,24 +13,22 @@ export default function PastEvents({
 }) {
   return (
     <div className="rounded-lg p-6 mt-16">
+      <p className="italic text-neutral-400">Past events</p>
       <TypographyH2 className="text-neutral-800">
         Kegiatan sebelumnya
       </TypographyH2>
-      <p className="italic text-neutral-400">Past events</p>
 
-      <div className="grid grid-cols-1 sm:grid-cols-2">
+      <div className="grid grid-cols-1 sm:grid-cols-2 gap-4">
         {events.map((event) => (
-          <Link key={`upcoming-event-${event.id}`} href={`/events/${event.id}`}>
-            <TypographyH3 className="text-lg font-semibold text-neutral-800">
-              {event.name}
-            </TypographyH3>
-          </Link>
+          <EventCard key={event.id} event={event} />
         ))}
       </div>
 
       {!hideSeeMoreButton && (
         <Link href="/events">
-          <Button variant="outline">Lihat semuanya</Button>
+          <Button variant="outline" className="mt-4">
+            Lihat semuanya
+          </Button>
         </Link>
       )}
     </div>
