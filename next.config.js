029f02a222b3c6c54490b38703447760ccc4f12a
Arief Rahmansyah <ariefrahmansyah@hotmail.com>
2024-09-18 23:02:54 +0700

    install newrelic

---
+++ next.config.js
@@ -0,0 +1,41 @@
+"use strict";
+
+const nrExternals = require("newrelic/load-externals");
+
+module.exports = {
+  experimental: {
+    serverComponentsExternalPackages: ["newrelic"],
+  },
+  webpack: (config) => {
+    nrExternals(config);
+    return config;
+  },
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "res.cloudinary.com",
+        port: "",
+        pathname: "/**",
+      },
+      {
+        protocol: "https",
+        hostname: "lh3.googleusercontent.com",
+        port: "",
+        pathname: "/**",
+      },
+      {
+        protocol: "https",
+        hostname: "i.ytimg.com",
+        port: "",
+        pathname: "/**",
+      },
+      {
+        protocol: "https",
+        hostname: "images.unsplash.com",
+        port: "",
+        pathname: "/**",
+      },
+    ],
+  },
+};
