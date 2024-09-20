6f9d4e96f27438a1a827ee3f9b60ab73c6509f44
Arief Rahmansyah <ariefrahmansyah@hotmail.com>
2024-09-20 20:29:35 +0700

    download certificate image (#103)
    Co-authored-by: Gading Nasution <account@gading.dev>

--- src/lib/canvas.ts
+++ src/lib/canvas.ts
@@ -1,77 +1,16 @@
-import html2canvas from "html2canvas";
+import { toPng } from "html-to-image";
 import { MutableRefObject } from "react";
-
-export async function getCanvasFromRef<T = HTMLElement>(
-  refElement: MutableRefObject<T>,
-  _scale = 5
-) {
-  const element = refElement.current as HTMLElement;
-  const w = element.clientWidth;
-  const h = element.clientHeight;
-
-  const _canvas = document.createElement("canvas");
-  _canvas.width = w * _scale;
-  _canvas.height = h * _scale;
-  _canvas.style.width = w + "px";
-  _canvas.style.height = h + "px";
-
-  const canvas = await html2canvas(element, {
-    canvas: _canvas,
-    scale: _scale,
-    useCORS: true,
-    allowTaint: true,
-    width: w,
-    height: h,
-  });
-
-  const ctx = canvas.getContext("2d") as any;
-  ctx.scale(_scale, _scale);
-  ctx.webkitImageSmoothingEnabled = false;
-  ctx.mozImageSmoothingEnabled = false;
-  ctx.imageSmoothingEnabled = false;
-
-  return canvas;
-}
-
-export async function getImageDataURL<T = HTMLElement>(
-  refElement: MutableRefObject<T>,
-  imgName: string
-) {
-  try {
-    const canvas = await getCanvasFromRef(refElement);
-    const canvasImgUrl = canvas.toDataURL("image/png", 1);
-    return canvasImgUrl;
-  } catch (error) {
-    throw new Error(`Something went wrong: ${error}`);
-  }
-}
 
 export async function downloadImage<T = HTMLElement>(
   refElement: MutableRefObject<T>,
   imgName: string
 ) {
-  const canvasImgUrl = await getImageDataURL(refElement, imgName);
-  const linkElement = document.createElement("a");
-  linkElement.download = imgName;
-  linkElement.href = canvasImgUrl;
-  linkElement.click();
-}
-
-export async function getImageBlob<T = HTMLElement>(
-  refElement: MutableRefObject<T>,
-  imgName: string
-): Promise<File> {
   try {
-    const canvas = await getCanvasFromRef(refElement);
-    return new Promise((res, rej) => {
-      canvas.toBlob((blob) => {
-        if (blob) {
-          const file = new File([blob], imgName, { type: blob.type });
-          res(file);
-        }
-        return rej(`Failed to convert Blob.`);
-      });
-    });
+    const canvas = await toPng(refElement.current as HTMLElement);
+    const linkElement = document.createElement("a");
+    linkElement.download = imgName;
+    linkElement.href = canvas;
+    linkElement.click();
   } catch (error) {
     throw new Error(`Something went wrong: ${error}`);
   }
