c13fc87b69d44fb8bca6d9bab30330e05808afe7
Gading Nasution <account@gading.dev>
2024-09-20 22:29:15 +0700

    fix: quality certificate

--- src/components/certificates/download.tsx
+++ src/components/certificates/download.tsx
@@ -1,9 +1,9 @@
 "use client";
 
-import { downloadImage } from "@/lib/canvas";
 import { useCallback, useRef } from "react";
 import { Button } from "../ui/button";
 import Certificate from "./certificate";
+import { downloadImage } from "@/lib/image";
 
 function CeriticateDownload() {
   const refCertificate = useRef<HTMLDivElement>(null);
