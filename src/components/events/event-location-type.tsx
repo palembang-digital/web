1ecee0cdc040bc91feae25e76e1eca8fd0281874
Arief Rahmansyah <ariefrahmansyah@hotmail.com>
2024-09-24 03:19:53 +0700

    event location type badge

---
+++ src/components/events/event-location-type.tsx
@@ -0,0 +1,25 @@
+import { Badge } from "@/components/ui/badge";
+import { cn } from "@/lib/utils";
+
+export default function EventLocationType({
+  type,
+  className,
+}: {
+  type: string;
+  className?: string;
+}) {
+  return (
+    <Badge
+      variant="outline"
+      className={cn(
+        "rounded-sm px-2",
+        type === "offline" && "border-red-100 bg-red-100 text-red-500 ",
+        type === "online" && "border-blue-100 bg-blue-100 text-blue-500",
+        type === "hybrid" && "border-green-100 bg-green-100 text-green-500",
+        className
+      )}
+    >
+      {type || "offline"}
+    </Badge>
+  );
+}
