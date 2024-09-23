0200fa634adc1e218d7492d4386d6403dc39649a
Arief Rahmansyah <ariefrahmansyah@hotmail.com>
2024-09-24 03:58:26 +0700

    add see more button

--- src/components/videos/videos.tsx
+++ src/components/videos/videos.tsx
@@ -1,11 +1,20 @@
+import { Button } from "@/components/ui/button";
 import { TypographyH2 } from "@/components/ui/typography";
 import YouTubeVideoCard from "@/components/youtube-video-card";
+import Link from "next/link";
 
-export default function Videos({ videos }: { videos: any[] }) {
+export default function Videos({
+  videos,
+  hideSeeMoreButton,
+}: {
+  videos: any[];
+  hideSeeMoreButton?: boolean;
+}) {
   return (
     <div className="p-6">
       <p className="italic text-neutral-400">Videos</p>
       <TypographyH2 className="text-neutral-800 mb-2">Galeri</TypographyH2>
+
       <div className="grid grid-cols-1 sm:grid-cols-3 gap-x-4 gap-y-6">
         {videos.map((video) => (
           <div
@@ -20,6 +29,14 @@ export default function Videos({ videos }: { videos: any[] }) {
           </div>
         ))}
       </div>
+
+      {!hideSeeMoreButton && (
+        <Link href="/gallery">
+          <Button variant="outline" className="mt-4 text-xs">
+            Lihat semua video
+          </Button>
+        </Link>
+      )}
     </div>
   );
 }
