65b6f41f1232363619742df38451d06f5b54a23a
Arief Rahmansyah <ariefrahmansyah@hotmail.com>
2024-09-27 20:50:24 +0700

    display who likes the post

--- src/app/for-you/post-card.tsx
+++ src/app/for-you/post-card.tsx
@@ -2,6 +2,7 @@
 
 import useMinimalTiptapEditor from "@/components/minimal-tiptap/hooks/use-minimal-tiptap";
 import "@/components/minimal-tiptap/styles/index.css";
+import { AnimatedTooltip } from "@/components/ui/animated-tooltip";
 import { Avatar, AvatarImage } from "@/components/ui/avatar";
 import { Button } from "@/components/ui/button";
 import { EditorContent } from "@tiptap/react";
@@ -61,6 +62,17 @@ export default function PostCard({
     feed.likes.length > 0 &&
     feed.likes.some((like: any) => like.userId === user.id);
 
+  const likedBy =
+    feed.likes &&
+    feed.likes.map(({ user }: { user: any }) => ({
+      id: user.id,
+      label: user.name,
+      image: user.image,
+      href: `/${user.username}`,
+    }));
+
+  const likedByShownLimit = 5;
+
   return (
     <div key={feed.id}>
       <div className="flex border p-4 rounded-md">
@@ -70,7 +82,19 @@ export default function PostCard({
         <div className="flex flex-col ml-2">
           <p className="text-xs font-bold">{feed.user.name}</p>
           <EditorContent className="minimal-tiptap-editor" editor={editor} />
-          <div className="flex gap-2 mt-2">
+          <div className="flex items-center">
+            {likedBy && (
+              <AnimatedTooltip items={likedBy.slice(0, likedByShownLimit)} />
+            )}
+            {likedBy.length > 0 && (
+              <p className="text-xs text-neutral-400 ml-3">
+                {likedBy.length > likedByShownLimit &&
+                  `+${likedBy.length} - ${likedByShownLimit} `}
+                liked this
+              </p>
+            )}
+          </div>
+          <div className="flex gap-2 mt-2 items-center">
             {isUserLiked ? (
               <Button variant="ghost" size="sm" onClick={unlikePost(feed.id)}>
                 <ThumbsUpIcon className="mr-2 h-4 w-4" />
