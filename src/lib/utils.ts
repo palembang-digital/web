aa46dbb887a13971ba55c247ab3032ff71bbb408
Arief Rahmansyah <ariefrahmansyah@hotmail.com>
2024-09-29 01:16:07 +0700

    use asia/jakarta as default timezone in server component

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -8,37 +8,59 @@ export function cn(...inputs: ClassValue[]) {
 
 export function getDay(date: Date): string {
   return DateTime.fromJSDate(date)
+    .setZone("Asia/Jakarta")
     .setLocale("id")
     .toLocaleString({ weekday: "long" });
 }
 
 export function getDate(date: Date): string {
-  return DateTime.fromJSDate(date).setLocale("id").toFormat("dd");
+  return DateTime.fromJSDate(date, { zone: "utc" })
+    .setZone("Asia/Jakarta")
+    .setLocale("id")
+    .toFormat("dd");
 }
 export function getMonth(date: Date): string {
-  return DateTime.fromJSDate(date).setLocale("id").toFormat("LLL");
+  return DateTime.fromJSDate(date)
+    .setZone("Asia/Jakarta")
+    .setLocale("id")
+    .toFormat("LLL");
 }
 
 export function getYear(date: Date): string {
-  return DateTime.fromJSDate(date).setLocale("id").toFormat("yyyy");
+  return DateTime.fromJSDate(date)
+    .setZone("Asia/Jakarta")
+    .setLocale("id")
+    .toFormat("yyyy");
 }
 
 export function getMonthYear(date: Date): string {
   return `${getMonth(date)} ${getYear(date)}`;
 }
 
-export function localeDate(date: Date, locale: string = "id"): string {
+export function localeDate(
+  date: Date,
+  zone: string = "Asia/Jakarta",
+  locale: string = "id"
+): string {
   return DateTime.fromJSDate(date)
+    .setZone(zone)
     .setLocale(locale)
     .toLocaleString(DateTime.DATE_FULL);
 }
 
-export function localeTime(date: Date, locale: string = "id"): string {
-  return DateTime.fromJSDate(date).setLocale(locale).toLocaleString({
-    hour: "2-digit",
-    minute: "2-digit",
-    timeZoneName: "short",
-  });
+export function localeTime(
+  date: Date,
+  zone: string = "Asia/Jakarta",
+  locale: string = "id"
+): string {
+  return DateTime.fromJSDate(date)
+    .setZone(zone)
+    .setLocale(locale)
+    .toLocaleString({
+      hour: "2-digit",
+      minute: "2-digit",
+      timeZoneName: "short",
+    });
 }
 
 export function getYoutubeVideoId(url: string): string {
