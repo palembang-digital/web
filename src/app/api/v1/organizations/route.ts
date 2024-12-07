877ffbfe19935bfeb7ad3b9fa150faec07a43dec
Arief Rahmansyah <ariefrahmansyah@hotmail.com>
2024-12-07 13:33:30 +0700

    Allow members to create org

--- src/app/api/v1/organizations/route.ts
+++ src/app/api/v1/organizations/route.ts
@@ -18,11 +18,6 @@ export async function POST(req: Request) {
     return Response.json({ message: "Not authenticated" }, { status: 401 });
   }
 
-  // @ts-ignore
-  if (session?.user?.role !== "administrator") {
-    return Response.json({ message: "Not authorized" }, { status: 403 });
-  }
-
   const data = await req.json();
 
   const newOrganization = await db
