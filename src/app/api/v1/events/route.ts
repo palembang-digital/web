2a3892cc3db6facf7e1c8c69eca39b7db903b313
Arief Rahmansyah <ariefrahmansyah@hotmail.com>
2024-12-07 17:45:40 +0700

    Allow members to create event

--- src/app/api/v1/events/route.ts
+++ src/app/api/v1/events/route.ts
@@ -29,11 +29,6 @@ export async function POST(req: Request) {
     return Response.json({ message: "Not authenticated" }, { status: 401 });
   }
 
-  // @ts-ignore
-  if (session?.user?.role !== "administrator") {
-    return Response.json({ message: "Not authorized" }, { status: 403 });
-  }
-
   const data = await req.json();
 
   const inputEvent = {
