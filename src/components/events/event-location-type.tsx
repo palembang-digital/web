e0247fbbf4c448e4cb6532a47941a9934ab834d8
Arief Rahmansyah <ariefrahmansyah@hotmail.com>
2024-12-16 21:17:53 +0700

    capitalize online/offline event type

--- src/components/events/event-location-type.tsx
+++ src/components/events/event-location-type.tsx
@@ -12,7 +12,7 @@ export default function EventLocationType({
     <Badge
       variant="outline"
       className={cn(
-        "rounded-sm px-2",
+        "rounded-sm px-2 capitalize text-xs",
         type === "offline" && "border-red-100 bg-red-100 text-red-500 ",
         type === "online" && "border-blue-100 bg-blue-100 text-blue-500",
         type === "hybrid" && "border-green-100 bg-green-100 text-green-500",
