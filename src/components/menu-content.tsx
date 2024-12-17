9c85b8ea92f7dc60c8724c2fa3f45bf633eb9921
Roli Bernanda <acappemenang@gmail.com>
2024-12-18 00:17:13 +0700

    feat: extract auth button into dedicated client component

--- src/components/menu-content.tsx
+++ src/components/menu-content.tsx
@@ -1,11 +1,10 @@
 import { NavigationLink } from "@/components/navigation-link";
-import { SignIn } from "@/components/sign-in";
 import { MENU_LINKS } from "@/lib/constants";
-import { Session } from "next-auth";
 import Image from "next/image";
 import Link from "next/link";
+import { AuthButton } from "./auth-button";
 
-export function MenuContent({ session }: { session: Session | null }) {
+export function MenuContent() {
   return (
     <div className="flex w-full flex-col text-sm">
       <div className="flex flex-col gap-4">
@@ -33,41 +32,9 @@ export function MenuContent({ session }: { session: Session | null }) {
               icon={link.icon}
             />
           ))}
-          {session ? (
-            <NavigationLink
-              // @ts-ignore
-              href={`/${session.user?.username}`}
-              label={session.user?.name || "Profil"}
-              icon={
-                <Image
-                  src={session.user?.image || ""}
-                  width={18}
-                  height={18}
-                  alt={session.user?.name || "Profile Picture"}
-                  className="rounded-full"
-                />
-              }
-            />
-          ) : (
-            <SignIn />
-          )}
+          <AuthButton />
         </div>
       </div>
-
-      {/* <hr />
-
-      <div className="flex flex-col gap-2 text-sm">
-        <div className="flex flex-col gap-1">
-          {Object.values(SOCIAL_LINKS).map((social) => (
-            <NavigationLink
-              key={social.url}
-              href={social.url}
-              label={social.title}
-              icon={social.icon}
-            />
-          ))}
-        </div>
-      </div> */}
     </div>
   );
 }
